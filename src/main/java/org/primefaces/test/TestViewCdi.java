d9e834c26ee827c063b27f3e442678bc089995be
Ravi Hariharan <rhariharan@leanindustries.com>
2018-12-19 12:54:01 -0500

    f:selectItems issue IE 11

---
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -0,0 +1,75 @@
+package org.primefaces.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.model.SelectItem;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class TestViewCdi
+{
+
+    private String           testString;
+
+    private String           console;
+
+    private String           console2;
+
+    private List<SelectItem> cars;
+
+    @PostConstruct
+    public void init()
+    {
+        testString = "Welcome to PrimeFaces (from CDI!) !!!";
+
+        cars = new ArrayList<SelectItem>();
+        cars.add(new SelectItem("5123456789012345", "5123456789012345"));
+        cars.add(new SelectItem("5123456789012346", "5123456789012346"));
+        cars.add(new SelectItem("5123456789012347", "5123456789012347"));
+    }
+
+    public String getTestString()
+    {
+        return testString;
+    }
+
+    public void setTestString(String testString)
+    {
+        this.testString = testString;
+    }
+
+    public String getConsole()
+    {
+        return console;
+    }
+
+    public void setConsole(String console)
+    {
+        this.console = console;
+    }
+
+    public List<SelectItem> getCars()
+    {
+        return cars;
+    }
+
+    public void setCars(List<SelectItem> cars)
+    {
+        this.cars = cars;
+    }
+
+    public String getConsole2()
+    {
+        return console2;
+    }
+
+    public void setConsole2(String console2)
+    {
+        this.console2 = console2;
+    }
+
+}
