17527538b324f0ec848d8391b37452a497cdd1e0
Ravi Hariharan <rhariharan@leanindustries.com>
2019-06-04 16:37:54 -0400

    Force SelectManyMenu to a String

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -1,34 +1,30 @@
 package org.primefaces.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.model.SelectItem;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
 public class TestViewCdi {
 
-    private List<String> selectedOptions;
+    private String console;
 
     @PostConstruct
     public void init() {
     }
 
-    public List<String> getSelectedOptions() {
-        return selectedOptions;
+    public String getConsole() {
+        return console;
     }
 
-    public void setSelectedOptions(List<String> selectedOptions) {
-        this.selectedOptions = selectedOptions;
+    public void setConsole(String console) {
+        this.console = console;
     }
 
     public void displayValue() {
         System.out.println("displayValue");
-        System.out.println(selectedOptions);
+        System.out.println(console);
 
     }
 }
