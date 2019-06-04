d3858cdbb3af96db087be12f03edce7e20ec0467
Ravi Hariharan <rhariharan@leanindustries.com>
2019-06-04 16:36:18 -0400

    Force SelectManyMenu to a String

--- src/main/java/org/primefaces/test/TestViewCdi.java
+++ src/main/java/org/primefaces/test/TestViewCdi.java
@@ -12,56 +12,10 @@ import javax.inject.Named;
 @RequestScoped
 public class TestViewCdi {
 
-    private String testString;
-
-    private String console;
-
-    private String console2;
-
-    private List<SelectItem> cars;
-
     private List<String> selectedOptions;
 
     @PostConstruct
     public void init() {
-        testString = "Welcome to PrimeFaces (from CDI!) !!!";
-
-        cars = new ArrayList<SelectItem>();
-        cars.add(new SelectItem("5123456789012345", "5123456789012345"));
-        cars.add(new SelectItem("5123456789012346", "5123456789012346"));
-        cars.add(new SelectItem("5123456789012347", "5123456789012347"));
-    }
-
-    public String getTestString() {
-        return testString;
-    }
-
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }
-
-    public String getConsole() {
-        return console;
-    }
-
-    public void setConsole(String console) {
-        this.console = console;
-    }
-
-    public List<SelectItem> getCars() {
-        return cars;
-    }
-
-    public void setCars(List<SelectItem> cars) {
-        this.cars = cars;
-    }
-
-    public String getConsole2() {
-        return console2;
-    }
-
-    public void setConsole2(String console2) {
-        this.console2 = console2;
     }
 
     public List<String> getSelectedOptions() {
@@ -76,6 +30,5 @@ public class TestViewCdi {
         System.out.println("displayValue");
         System.out.println(selectedOptions);
 
-        System.out.println("console " + console);
     }
 }
